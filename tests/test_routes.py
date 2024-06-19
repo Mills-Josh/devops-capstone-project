d6ccae0c72a051a13429031da08cb5431e64a08d
Joshua Mills <jrmills@arizona.edu>
2024-06-19 14:13:42 -0400

    Create tests for delete

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -173,3 +173,13 @@ class TestAccountService(TestCase):
         """It should return 404 when updating invalid id"""
         response = self.client.put(f"{BASE_URL}/0")
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_delete(self):
+        """It should delete the account"""
+        account = self._create_accounts(1)[0]
+
+        response = self.client.delete(f"{BASE_URL}/{account.id}")
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+        response = self.client.delete(f"{BASE_URL}/{account.id}")
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
